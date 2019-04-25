d1bd21535ef37666d2d9652e0a8591f43eb90e9d
Taryn Laramie <taryn@fullautomedia.com>
2019-04-25 11:45:45 -0600

    add ngrx devtools

--- apps/aco/src/app/app.module.ts
+++ apps/aco/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './+state';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, StoreModule.forRoot(reducers, { metaReducers })],
+  imports: [BrowserModule, StoreModule.forRoot(reducers, { metaReducers }), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
