3af5eb2655f3e282c807997c4a2aeca1e1d489b4
Taryn Laramie <taryn@fullautomedia.com>
2019-04-25 11:33:43 -0600

    add ngrx

--- apps/aco/src/app/app.module.ts
+++ apps/aco/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './+state';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, StoreModule.forRoot(reducers, { metaReducers })],
   providers: [],
   bootstrap: [AppComponent]
 })
